487a832c9736704406a642466ce1193d000d387f
vicgragil <vicgragil@alum.us.es>
2021-12-17 17:43:35 +0100

    feat: Fechas de las jornadas

--- src/handlers.py
+++ src/handlers.py
@@ -1,6 +1,8 @@
 from commands.start import start
 from commands.evidencias import evidencias
+from commands.fechas import fechas
 from telegram.ext import CommandHandler
 def setUpHandlers(bot):
     bot.add_handler(CommandHandler('start',	start))
     bot.add_handler(CommandHandler('evidencias', evidencias))
+    bot.add_handler(CommandHandler('fechas', fechas))
