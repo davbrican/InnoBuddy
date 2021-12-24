864ba8a8c7dbb0f8dfc82fdcfce7f7eb877f5f59
juanramonuvus <72406617+juanramonuvus@users.noreply.github.com>
2021-12-24 01:30:55 +0100

    feat: Añadir menu con comandos
    Añade un menú en la parte inferior del bot que enseña todos los posibles comandos que puedes utilizar, además poniendo en el cuadro de texto el carácter "/" te enseña también todos los comandos

--- src/commands/start.py
+++ src/commands/start.py
@@ -1,7 +1,10 @@
 import json
 import os
 from commands.readMesssage import readMessage
+from telegram import KeyboardButton, ReplyKeyboardMarkup
+from telegram.ext import CommandHandler, dispatcher
 
 def start(update, context):
+
     mensaje = readMessage("start")
     context.bot.send_message(update.message.chat_id, mensaje, parse_mode='MarkdownV2')
--- src/handlers.py
+++ src/handlers.py
@@ -1,12 +1,17 @@
+from telegram.botcommand import BotCommand
+from telegram.botcommandscope import BotCommandScopeDefault
 from telegram.callbackquery import CallbackQuery
 from commands.start import start
 from commands.evidencias import evidencias
 from commands.valoraciones import responder_valoraciones
 from commands.fechas import fechas
-from commands.socialMedias import socialMedias
+from commands.socialMedias import socialmedias
 
 from telegram.ext import CommandHandler
 from telegram.ext import CallbackQueryHandler
+
+from telegram import BotCommandScope
+
 
 
 def setUpHandlers(bot):
@@ -14,5 +19,13 @@ def setUpHandlers(bot):
     bot.add_handler(CommandHandler('evidencias', evidencias))
     bot.add_handler(CallbackQueryHandler(responder_valoraciones))
     bot.add_handler(CommandHandler('fechas', fechas))
-    bot.add_handler(CommandHandler('socialMedias', socialMedias))
-
+    bot.add_handler(CommandHandler('socialmedias', socialmedias))
+    
+def settingCommands(bot):
+    command = [
+      BotCommand(command='/socialmedias', description= 'Ver nuestras redes sociales'),
+      BotCommand(command='/evidencias', description ='Información acerca de las evidencias'),
+      BotCommand(command='/fechas', description='Enterate de las fechas de las charlas y eventos')
+   ]
+    bot.setMyCommands(commands=command)
+    bot.getMyCommands()
