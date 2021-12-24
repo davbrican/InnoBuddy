ea1c03d822d530696b23199bbe513df5364d2fb3
vicgragil <vicgragil@alum.us.es>
2021-12-24 19:28:58 +0100

    feat: Comando Mapa
    Añadida la funcionalidad de mostrar un mapa con un localizador en un aula específica.

--- src/handlers.py
+++ src/handlers.py
@@ -4,6 +4,7 @@ from commands.evidencias import evidencias
 from commands.valoraciones import responder_valoraciones
 from commands.fechas import fechas
 from commands.socialMedias import socialMedias
+from commands.localizacion import localizacion
 
 from telegram.ext import CommandHandler
 from telegram.ext import CallbackQueryHandler
@@ -15,4 +16,5 @@ def setUpHandlers(bot):
     bot.add_handler(CallbackQueryHandler(responder_valoraciones))
     bot.add_handler(CommandHandler('fechas', fechas))
     bot.add_handler(CommandHandler('socialMedias', socialMedias))
+    bot.add_handler(CommandHandler('localizacion', localizacion))
 
