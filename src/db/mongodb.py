b4e55f39167b117ec6f57e9526251f41cb67a811
davbrican <72500941+davbrican@users.noreply.github.com>
2021-12-24 02:51:56 +0100

    feat: mongodb conexion creada

---
+++ src/db/mongodb.py
@@ -0,0 +1,19 @@
+import pymongo
+import os
+from dotenv import load_dotenv
+import ssl
+from datetime import datetime
+from utils import load_mongo
+    
+def get_all_data():
+    collection = load_mongo()
+    return collection.find()
+
+def get_by_titulo(entrance):
+    collection = load_mongo()
+    return collection.find_one({"titulo": entrance})
+    
+def get_by_day(entrance):
+    collection = load_mongo()
+    end = datetime(entrance.year, entrance.month, entrance.day, 23, 0, 0)
+    return collection.find({"inicio": {"$gte": entrance, "$lt": end}})
--- src/db/utils.py
+++ src/db/utils.py
@@ -1,6 +1,8 @@
 import mysql.connector
 import os
 from dotenv import load_dotenv
+import pymongo
+import ssl
 
 load_dotenv()
 
@@ -12,3 +14,12 @@ def connect():
     database=os.getenv("MYSQL_DB")
     )
     return conn
+
+def load_mongo():
+    client = pymongo.MongoClient(os.getenv('LINK_MONGO'), ssl_cert_reqs=ssl.CERT_NONE)
+
+    #EJEMPLO PARA IMPRIMIR TODOS LOS ENVENTOS DE LA BASE DE DATOS
+    db = client['innosoftEventia']
+    collection = db['peticionpublicacions']
+    
+    return collection
